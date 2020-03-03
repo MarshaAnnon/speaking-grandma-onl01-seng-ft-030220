1271b65dd42d81de1b8f3afd2d5140f421063ecc
Marsha Annon <marshaannon@gmail.com>
2020-03-03 18:25:48 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,3 +1,7 @@
+def speak_to_grandma
+if speak_to_grandma == "I LOVE YOU GRANDMA!"
+  puts "I LOVE YOU TOO PUMPKIN!"
+end
 # Write a speak_to_grandma method.
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
